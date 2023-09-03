9733ee0e219d7188ae95c1be36196eb73dfb2e7e
Loh Silva <lo-ruama_sp@hotmail.com>
2023-09-02 21:20:33 -0300

    Consulta simples com Query Methods

--- src/main/java/com/course/workshopmongodb/services/PostService.java
+++ src/main/java/com/course/workshopmongodb/services/PostService.java
@@ -1,12 +1,12 @@
 package com.course.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.workshopmongodb.domain.Post;
-import com.course.workshopmongodb.domain.User;
 import com.course.workshopmongodb.repository.PostRepository;
 import com.course.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -19,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = rep.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle (String text){
+		return rep.findByTitleContainingIgnoreCase(text);
 	}
 
 }
