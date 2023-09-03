375d69f5ff0db7cb21dda46bfed704dacfed51c3
Loh Silva <lo-ruama_sp@hotmail.com>
2023-09-02 22:00:22 -0300

    Consulta com vários critérios

--- src/main/java/com/course/workshopmongodb/services/PostService.java
+++ src/main/java/com/course/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.course.workshopmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,9 +21,14 @@ public class PostService {
 		Optional<Post> obj = rep.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public List<Post> findByTitle (String text){
+
+	public List<Post> findByTitle(String text) {
 		return rep.findByTitle(text);
+	}
+
+	public List<Post> searchFull(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return rep.searchFull(text, minDate, maxDate);
 	}
 
 }
